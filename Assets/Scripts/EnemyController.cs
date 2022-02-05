58a1e40a0b12e4d0cc92d63f0cdc55a5e503f8af
suke0806 <hondii1997@gmail.com>
2022-02-06 00:36:58 +0900

    ほぼ完成

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -11,9 +11,14 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private float limitSpeed;      //弾の制限速度
     private Rigidbody2D rb;                         //弾のRigidbody2D
     private Transform bulletTrans;                  //弾のTransform
+    Collider2D co2D;
 
     // GameObject GameResult;
     GameManager gameManager;
+
+    float delta = 0;
+    float vanishTime = 8.0f; // スポーンしてから消滅するまでの時間
+    float invincibleTime = 1.0f; // スポーン時の無敵時間
 
     // Start is called before the first frame update
     void Start()
@@ -21,14 +26,30 @@ public class EnemyController : MonoBehaviour
         player = GameObject.Find("ore");
         rb = GetComponent<Rigidbody2D>();
         bulletTrans = GetComponent<Transform>();
-
-        // this.GameResult = GameObject.Find("GameResult");
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        // 最初の無敵時間だけ透ける判定に
+        this.co2D = GetComponent<Collider2D>();
+        this.co2D.isTrigger = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        // 時間経過
+        this.delta += Time.deltaTime;
+
+        // 時間経過で無敵解除
+        if (this.delta > this.invincibleTime){
+            this.co2D.isTrigger = false;
+        }
+
+        // 時間経過で消滅
+        if (this.delta > this.vanishTime){
+            Destroy(gameObject);
+        }
+
         Vector3 vector3 = player.transform.position - bulletTrans.position;  //弾から追いかける対象への方向を計算
         rb.AddForce(vector3.normalized * bulletSpeed);                  //方向の長さを1に正規化、任意の力をAddForceで加える
 
@@ -38,16 +59,14 @@ public class EnemyController : MonoBehaviour
     }
 
     // ゲームオーバー判定
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        // クリアしてなければ
-        if (!gameManager.GameClearFlag){
-
-            // プレイヤーに衝突したらゲームオーバー
-            if (collision.gameObject.name == "ore"){
-                // this.GameResult.GetComponent<Text>().text = "GAME OVER...";
-                gameManager.GameOver();
+        // if (this.delta > this.invincibleTime){ // 無敵時間を過ぎていれば
+            if (!gameManager.GameClearFlag){ // クリアしてなければ
+                if (collision.gameObject.name == "ore"){ // プレイヤーに衝突したら
+                    gameManager.GameOver(); // ゲームオーバー
+                }
             }
-        }
+        
     }
 }
--- Assets/Scripts/EnemyGenerator.cs
+++ Assets/Scripts/EnemyGenerator.cs
@@ -6,14 +6,29 @@ using UnityEngine.UI;
 public class EnemyGenerator : MonoBehaviour
 {
     public GameObject enemyPrefab_1;
-    public GameObject enemyPrefab_2;
-    int[] schedule_1 = {0, 365*4};
-    int[] schedule_2 = {365*2, 365*3};
+    int[] schedule_1 = {0, 365*3};
     [SerializeField] private float span_1;
-    [SerializeField] private float span_2;
-
     float delta_1 = 0;
+
+    public GameObject enemyPrefab_2;
+    int[] schedule_2 = {365*2, 365*3};
+    [SerializeField] private float span_2;
     float delta_2 = 0;
+
+    public GameObject enemyPrefab_3;
+    int[] schedule_3 = {0, 365*3};
+    [SerializeField] private float span_3;
+    float delta_3 = 0;
+
+    public GameObject enemyPrefab_4;
+    int[] schedule_4 = {0, 365*3};
+    [SerializeField] private float span_4;
+    float delta_4 = 0;
+
+    public GameObject enemyPrefab_5;
+    int[] schedule_5 = {365*3, 365*4};
+    [SerializeField] private float span_5;
+    float delta_5 = 0;
 
     float x_lim;
     float y_lim;
@@ -33,18 +48,24 @@ public class EnemyGenerator : MonoBehaviour
         y_lim = y_lim - 0.5f;
     }
 
+    // 時間同期
     void TimeSync()
     {
         TimeController timeCon = GameObject.Find("GameManager").GetComponent<TimeController>();
         progress_date = timeCon.progress_date;
     }
 
-    void Start()
-    {
-        SetScreenVertices();
-        TimeSync();
+    // 初期スポーン時間をランダムにずらす
+    void RandomSeed(){
+        
+        this.delta_1 = Random.Range(0.0f, 1.0f);
+        this.delta_2 = Random.Range(0.0f, 1.0f);
+        this.delta_3 = Random.Range(0.0f, 1.0f);
+        this.delta_4 = Random.Range(0.0f, 1.0f);
+        this.delta_5 = Random.Range(0.0f, 1.0f);
     }
 
+    // ランダムな位置にスポーン
     (float x, float y) RandomPosition(){
         float px = Random.Range(-x_lim, x_lim);
         float py = Random.Range(-y_lim, y_lim);
@@ -52,36 +73,58 @@ public class EnemyGenerator : MonoBehaviour
         return (x: px, y: py);
     }
 
-    void Sporn(GameObject enemyPrefab, int[] schedule)
+    // スポーン
+    void Spawn(GameObject enemyPrefab, int[] schedule)
     {
         TimeSync(); // 時間同期
-
-        // スケジュールが合っていたら敵生成
+        // スケジュールが合っていたらスポーン
         if (schedule[0] <= this.progress_date & this.progress_date < schedule[1]){
-
-            Debug.Log(schedule);
-            Debug.Log(progress_date);
-
             GameObject enemy = Instantiate(enemyPrefab);
             var pos = RandomPosition();
             enemy.transform.position = new Vector3(pos.x, pos.y, 0);
         }
     }
 
+    void Start()
+    {
+        SetScreenVertices();
+        TimeSync();
+        RandomSeed();
+    }
+
     void Update()
     {
-        
+        // 時間経過
         this.delta_1 += Time.deltaTime;
         this.delta_2 += Time.deltaTime;
+        this.delta_3 += Time.deltaTime;
+        this.delta_4 += Time.deltaTime;
+        this.delta_5 += Time.deltaTime;
 
+        // スポーン
         if (this.delta_1 > this.span_1){
-            Sporn(enemyPrefab_1, schedule_1);
+            Spawn(enemyPrefab_1, schedule_1);
             this.delta_1 = 0;
         }
 
         if (this.delta_2 > this.span_2){
-            Sporn(enemyPrefab_2, schedule_2);
+            Spawn(enemyPrefab_2, schedule_2);
             this.delta_2 = 0;
+        }
+
+        if (this.delta_3 > this.span_3){
+            Spawn(enemyPrefab_3, schedule_3);
+            this.delta_3 = 0;
+        }
+
+        if (this.delta_4 > this.span_4){
+            Spawn(enemyPrefab_4, schedule_4);
+            this.delta_4 = 0;
+        }
+
+        if (this.delta_5 > this.span_5){
+            Spawn(enemyPrefab_5, schedule_5);
+            this.delta_5 = 0;
         }
         
     }
